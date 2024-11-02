a6e813919dae169164756cf4bd184634d004de4c
Veronika <94937684+aeterraes@users.noreply.github.com>
2024-11-02 22:39:08 +0300

    lab7

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,21 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".gallery-image");
-    let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-    images[currentIndex].classList.add("active");
-
-    function showNextImage() {
-        images[currentIndex].classList.remove("active");
-        currentIndex = (currentIndex + 1) % images.length;
-        images[currentIndex].classList.add("active");
-    }
-
-    function showPrevImage() {
-        images[currentIndex].classList.remove("active");
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        images[currentIndex].classList.add("active");
-    }
-
-    document.getElementById("next").addEventListener("click", showNextImage);
-    document.getElementById("prev").addEventListener("click", showPrevImage);
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        slidesPerView: 1,
+        spaceBetween: 30,
+    });
 });
